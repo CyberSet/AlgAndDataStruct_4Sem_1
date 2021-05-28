63ccf04722b150872aa9399cada5343f92406399
CyberSet <sashapiter2015@gmail.com>
2021-05-28 20:06:26 +0300

    new insert

--- map.h
+++ map.h
@@ -22,26 +22,9 @@ public:
 		}
 		else{
 			node* newNode = new node(node_key, node_value, root);
-			while (true) {
-				if (newNode > newNode->getParent())  if (newNode->getParent()->getRightChild() != nullptr) newNode->setParent(newNode->getRightChild());
-				else break;
-				else if (newNode->getParent()->getLeftChild() != nullptr) newNode->setParent(newNode->getLeftChild());
-				else break;
-			}
-			if (newNode > newNode->getParent()) newNode->getParent()->setRightChild(newNode);
-			else newNode->getParent()->setLeftChild(newNode);
-			while (!newNode->getParent()->isRed()) {
-				parent = newNode->getParent();
-				grandParent = parent->getParent();
-				if (parent < grandParent) {
-					if (grandParent->getRightChild()->isRed()) {
-						grandParent->getLeftChild()->setColor(black);
-						grandParent->getRightChild()->setColor(black);
-						grandParent->setColor(red);
-						newNode = grandParent;
-					}
-				}
-			}
+			node* y = nullptr;
+			node* x = root;
+			while()
 		}
 	}
 };
